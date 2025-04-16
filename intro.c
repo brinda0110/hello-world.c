2c807624b652b1e0b56b66d088b97e6eb01d141d
brinda0110 <brindagoyani01@gmail.com>
2025-04-16 10:09:24 +0100

    Add files via upload

---
+++ intro.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	printf("name:brinda\n");
+	printf("surname:goyani\n");
+	printf("fathername:nayanbhai\n");
+	printf("mothername:dharaben\n");
+	printf("hobby:drawing\n");
+	
+}
---
+++ pattern2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("-----------\n");
+	printf("|         | \n");
+	printf("|         | \n");
+	printf("| brinda  |\n");
+	printf("|         | \n");
+	printf("|         | \n");
+	printf("-----------\n");
+}
