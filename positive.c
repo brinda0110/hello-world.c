38b32c3daa5a2405149e3abf1e0cf33c874c396e
brinda0110 <brindagoyani01@gmail.com>
2025-04-18 17:46:46 +0530

    classwork

---
+++ positive.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int a,b ;
+	printf("enter value of a:");
+	scanf("%d",&a);
+	printf("enter value of b:");
+	scanf("%d",&b);
+
+ if (a>0)
+ { 
+   printf("a is positive");
+   
+ }
+else{
+	printf("b is negative");
+} 
+}
