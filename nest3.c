9ecd8567c6b426c075c00ad2b22d436958832491
brinda0110 <brindagoyani01@gmail.com>
2025-04-19 14:40:30 +0530

    classwork

---
+++ nest3.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+	int a=63 ,b=0,c=1,d=4, e=7 ;
+	if(a<b)
+          { if(a<c)
+          
+                {if (a<d)
+                        {if(a<e) {printf("a is Min");
+						}
+						else{printf("e is min");
+						}} 
+	
+		else{ printf("d is Min");
+		}  	  
+		       }  else{ printf("c is min");
+			   }
+		  } 
+	else 
+	    { if(b<c)
+		         {if(b<d)
+		                 {if(b<e) {printf("b is Min");
+						 }
+						 else{ printf("e is Min");
+						 }
+						 }else
+						 { printf("d is min");
+						                        }
+						 }else{ printf("c is min");
+						 }
+				 }
+		  }  
+		   
+		  
+		  
+	                         	
+	
