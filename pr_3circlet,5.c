7364d69e13ed490dd33798bdc2f339b58474062a
brinda0110 <brindagoyani01@gmail.com>
2025-04-30 16:22:40 +0530

    project work 3

---
+++ pr_3circlet,5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+ {
+ 	int i,j,k;
+ 	for(i=1;i<=5;i++)
+	 {
+	 	 for(k=1;k<=5-i;k++)
+	 	{
+	 		printf("  ");
+		 }
+		 for(j=i;j<=2*i-1;j++)
+		 {
+		 	printf("%d ",j);
+		  }
+		  for(j=2*i-2;j>=i;j--)
+		 {
+		 	printf("%d ",j);
+		  }
+		
+		 printf("\n");
+	 }
+ }
